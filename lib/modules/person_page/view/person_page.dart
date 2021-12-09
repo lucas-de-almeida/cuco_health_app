895e6dd98c9b4711f3d6b79c248741940dbe9e52
lucas <lucasde.almeida@hotmail.com.com>
2021-12-09 19:00:50 -0300

    alterada tela para suporte ao widgets criado (PersonCard)

--- lib/modules/person_page/view/person_page.dart
+++ lib/modules/person_page/view/person_page.dart
@@ -1,4 +1,6 @@
+import 'package:cuco_health_app/models/person.dart';
 import 'package:cuco_health_app/modules/person_page/controller/person_controller.dart';
+import 'package:cuco_health_app/modules/person_page/view/widgets/person_card.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,11 +9,38 @@ class PersonPage extends GetView<PersonController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('PersonPage'),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFF2296F3),
+        body: Container(
+          width: Get.width,
+          height: Get.height,
+          child: controller.obx(
+            (value) => ListView.builder(
+              itemCount: controller.listPerson.length,
+              itemBuilder: (_, index) {
+                Person _person = controller.listPerson[index];
+                return PersonCard(person: _person);
+              },
+            ),
+            onLoading: Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            ),
+            onError: (value) => Expanded(
+              child: Center(
+                child: Text('$value'),
+              ),
+            ),
+            onEmpty: const Expanded(
+              child: Center(
+                child: Text('Sem informações para mostrar'),
+              ),
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
