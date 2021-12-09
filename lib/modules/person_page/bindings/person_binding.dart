8cd4f166871892fc6f94b8ac57f5d580e429d0e7
lucas <lucasde.almeida@hotmail.com.com>
2021-12-09 19:01:40 -0300

    arquivos suplementares a person page

--- lib/modules/person_page/bindings/person_binding.dart
+++ lib/modules/person_page/bindings/person_binding.dart
@@ -1,6 +1,9 @@
+import 'package:cuco_health_app/modules/person_page/controller/person_controller.dart';
 import 'package:get/get.dart';
 
 class PersonBinding implements Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.put(PersonController(), permanent: true);
+  }
 }
--- lib/modules/person_page/controller/person_controller.dart
+++ lib/modules/person_page/controller/person_controller.dart
@@ -1,3 +1,66 @@
+import 'package:cuco_health_app/aplication/repository/person_repository/person_repository.dart';
+import 'package:cuco_health_app/aplication/rest_client/rest_client.dart';
+import 'package:cuco_health_app/aplication/ui/messages/messages_mixin.dart';
+import 'package:cuco_health_app/models/person.dart';
 import 'package:get/get.dart';
 
-class PersonController extends GetxController {}
+RestClient rClient = RestClient();
+
+class PersonController extends GetxController with MessagesMixin, StateMixin {
+  var personRepository = PersonRepository(restClient: rClient);
+
+  final message = Rxn<MessageModel>();
+
+  final listPerson = <Person>[].obs;
+
+  final _totalLista = 0.obs;
+  int get totalLista => _totalLista.value;
+  set totalLista(int newValue) => _totalLista(newValue);
+
+  @override
+  void onInit() {
+    super.onInit();
+    messageListener(message);
+
+    listPerson.clear();
+  }
+
+  @override
+  void onReady() async {
+    initialCall();
+
+    super.onReady();
+  }
+
+  Future<void> initialCall() {
+    return getPerson().then(
+      (value) {
+        if (value!.isNotEmpty) {
+          change(value, status: RxStatus.success());
+        } else {
+          change(null, status: RxStatus.empty());
+        }
+      },
+    );
+  }
+
+  Future<List<Person?>?> getPerson() async {
+    List<Person?>? person = [];
+    try {
+      person = await personRepository.findPerson();
+
+      if (person != null) {
+        for (var element in person) {
+          print(element!.name);
+          listPerson.add(element);
+        }
+      }
+
+      return person;
+    } catch (e, s) {
+      message(MessageModel.error(
+          title: 'Alerta', message: 'Falha ao buscar lista de amigos'));
+    }
+    return null;
+  }
+}
